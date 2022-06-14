a1ebdd199e5ff5ed18f6fd10270b9af9db5907a1
ddelena <89658366303@mail.ru>
2022-06-14 16:16:49 +0300

    init: sequelize for drom

---
+++ Photos.js
@@ -0,0 +1,30 @@
+import db from '../db'
+import {Model, DataTypes} from "sequelize";
+
+class Photos extends Model {
+
+}
+
+const model = Photos.init({
+    id: {
+        type: DataTypes.BIGINT(20).UNSIGNED,
+        primaryKey: true,
+        unique: true
+    },
+    url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    alt: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    car_id: {
+        type: DataTypes.BIGINT(20).UNSIGNED,
+        foreignKey: true
+    },
+    sequelize: db,
+    tableName: 'Photos'
+});
+
+export default model;
