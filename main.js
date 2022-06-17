011904542442c5276bad0753979dd77c078ccb95
ddelena <89658366303@mail.ru>
2022-06-17 17:54:48 +0300

    refactor: changing imports

--- main.js
+++ main.js
@@ -1,39 +1,30 @@
 import { openConnection, closeConnection } from "./db.js";
 import { runMigrations } from "./migration.js";
-import Cars from "./models/Cars.js";
 import db from "./db";
-// import Body_Type from './Body_Type';
-// import Contacts from './Contacts';
-// import Drive_Unit from './Drive_Unit';
-// import Photos from './Photos';
-// import Cities from './Cities';
-// import Markas from './Markas';
-// import transmission from './transmission';
+import Cars from "./models/Cars.js";
+import Body_Type from "./models/Body_Type";
+import Contacts from "./models/Contacts";
+import Drive_Unit from "./models/Drive_Unit";
+import Photos from "./models/Photos";
+import Cities from "./models/Cities";
+import Markas from "./models/Markas";
+import transmission from "./models/transmission";
 
 
 async function bootstrap() {
     try {
         await openConnection();
-
         await runMigrations();
-
         console.info('Connected');
-
-
         await db.transaction(async () => {
-            const cars = new Cars({spec_card: true, engine: 'Admin', year: 'Admin',
-                price: 'Admin', color: 'Admin', mileage: 'Admin'});
+            const cars = new Cities({
+                name: 'Kazan'
+            });
             await cars.save();
-    })
+        })
     } catch (err) {
         console.error(err);
     }
-
-        // const projects = [
-        //     {name: 'Project 1', user_id: user.id},
-        //     {name: 'Project 2', user_id: user.id}
-        // ];
-        // await Project.bulkCreate(projects);
 }
 
-bootstrap();
+bootstrap()
