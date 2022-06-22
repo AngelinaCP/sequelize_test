74fe39ac535ef808051af2737a9135ca1d1e9b74
AngelinaCP <89658366303@mail.ru>
2022-06-22 19:00:48 +0300

    feat: inserting in two tables with different statements

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -6,7 +6,9 @@ import Drive_Unit from "../models/Drive_Unit";
 import Photos from "../models/Photos";
 import Cities from "../models/Cities";
 import Markas from "../models/Markas";
-import sequelize from "sequelize";
+import sequelize, {DataTypes} from "sequelize";
+import models from "../models/Models";
+import {re} from "@babel/core/lib/vendor/import-meta-resolve";
 
     exports.getAllPosts = async (req, res, next) => {
         try {
@@ -24,48 +26,35 @@ import sequelize from "sequelize";
             return res.status(500).json(error);
         }
     };
-const photo_ = Photos.belongsTo(Cars, {foreignKey: 'car_id'});
-const city_ = Cities.belongsTo(Cars, {foreignKey: 'car_id'});
+
     exports.createOnePost = async (req, res, next) => {
         try {
-            // const CAR_MODEL = {
-            //     spec_card: req.body.spec_card,
-            //     engine: req.body.engine,
-            //     price: req.body.price,
-            //     color: req.body.color,
-            //     mileage: req.body.mileage,
-            //     year: req.body.year
-            // };
+            const CAR_MODEL = {
+                spec_card: req.body.spec_card,
+                engine: req.body.engine,
+                price: req.body.price,
+                color: req.body.color,
+                mileage: req.body.mileage,
+                year: req.body.year,
+                // Cities: {
+                //     name: req.body.city
+                // }
+            };
+            const CITY_MODEL = {
+                id:1 ,
+                name: req.body.city,
+                car_id: req.body.id
+            }
             try {
-                console.log('Car created');
-                // const car = await Cars.create(CAR_MODEL, {include:[{association: 'Drive_Unit'},
-                //         {association: 'Photos'}, {association: 'Body_Type'}, {association: 'Contacts'},
-                //         {association: 'Cities'}, {association: 'Transmission'},
-                //         {association: 'Markas', include: [{association: 'Models'}]}]});
-                const result = await sequelize.transaction (async(t) => {
-                    const cars = Cars.create({
-                        spec_card: req.body.spec_card,
-                        engine: req.body.engine,
-                        price: req.body.price,
-                        color: req.body.color,
-                        mileage: req.body.mileage,
-                        year: req.body.url })
-
-                    const cities_id = Cities.create({
-                        name: req.body.city,
-                        car_id: cars.id
-                    })
-                    return {
-                        cars,
-                        Cities: cities_id
-                    }
-                })
-                // ) .then(data => {
-                //     res.json({url: req.body.url, alt: req.body.alt})
-                // })
-
-                console.log('city  ' + req.body.city)
-                console.log(car)
+                // console.log('Car created');
+                const car = await Cars.create(
+                    CAR_MODEL
+                    // ,
+                    // {include: models.Cities}
+                );
+                const cities = await Cities.create(
+                    CITY_MODEL
+                )
                 res.send('Good job')
                 // return res.status(201).json(car);
             } catch (error) {
