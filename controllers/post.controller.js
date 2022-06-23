92ec905229f9584e8d7936a271561605540db7eb
AngelinaCP <89658366303@mail.ru>
2022-06-23 11:42:25 +0300

    still not working

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -29,35 +29,37 @@ import {re} from "@babel/core/lib/vendor/import-meta-resolve";
 
     exports.createOnePost = async (req, res, next) => {
         try {
+
+            let {spec_card, engine, price, color, mileage, year, photo} = req.body
             const CAR_MODEL = {
-                spec_card: req.body.spec_card,
-                engine: req.body.engine,
-                price: req.body.price,
-                color: req.body.color,
-                mileage: req.body.mileage,
-                year: req.body.year,
-                // Cities: {
-                //     name: req.body.city
-                // }
+                spec_card,
+                engine,
+                price,
+                color,
+                mileage,
+                year,
+                Cities: {
+                    name: req.body.city
+                }
             };
-            const CITY_MODEL = {
-                id:1 ,
-                name: req.body.city,
-                car_id: req.body.id
-            }
+            // const CITY_MODEL = {
+            //     name: req.body.city,
+            //     car_id: req.body.id
+            // }
             try {
                 // console.log('Car created');
                 const car = await Cars.create(
                     CAR_MODEL
-                    // ,
-                    // {include: models.Cities}
+                    ,
+                    {include: models.Cities}
                 );
-                const cities = await Cities.create(
-                    CITY_MODEL
-                )
+                // const cities = await Cities.create(
+                //     CITY_MODEL
+                // )
                 res.send('Good job')
                 // return res.status(201).json(car);
             } catch (error) {
+                console.log(error)
                 return res.status(500).json(error);
             }
         } catch (error) {
